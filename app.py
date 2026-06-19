181c097688265f0f0ba9642b2bee8176cc828314
Gallen903 <gallen@businesspost.ie>
2026-06-19 11:57:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -274,6 +274,7 @@ def init_db_with_defaults():
         # --- UK ---
         ("VOD.L","Vodafone Group","UK","GBp"),
         ("DCC.L","DCC plc","UK","GBp"),
+        ("DGE.L","Diageo plc","UK","GBp"),
         ("GNC.L","Greencore Group plc","UK","GBp"),
         ("GFTU.L","Grafton Group plc","UK","GBp"),
         ("HVO.L","hVIVO plc","UK","GBp"),
